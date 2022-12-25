3c725a020f042b7bfa1adf51d39741b3717c2607
mikajed <michaeljedrz@gmail.com>
2022-12-25 18:19:17 +0100

    changed from var to let

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-var span = document.getElementById('date');
+let span = document.getElementById('date');
 
 function time() {
-    var d = new Date();
-    var s = d.getSeconds();
-    var m = d.getMinutes();
-    var h = d.getHours();
+    let d = new Date();
+    let s = d.getSeconds();
+    let m = d.getMinutes();
+    let h = d.getHours();
     span.textContent = 
         ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
 }
