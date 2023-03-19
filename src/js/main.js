2e99f75b4917bc8088dec8a426ddad369bd58bda
mikajed <michaeljedrz@gmail.com>
2023-03-19 07:22:13 +0100

    add greetig based on daytime

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,16 @@ function myTimer() {
     let date = new Date();
     span.innerText = date.toLocaleTimeString();
 }
+
+// document.getElementById('greeting').innerHTML = new Date().getHours() = 12 ? 'Guten Tag' : 'Guten Morgen';
+
+const jetzt = new Date();
+const stunde = jetzt.getHours();
+
+if (stunde >= 6 && stunde < 12) {
+  document.getElementById("greeting").textContent = "Guten Morgen";
+} else if (stunde >= 12 && stunde < 18) {
+  document.getElementById("greeting").textContent = "Guten Tag";
+} else {
+  document.getElementById("greeting").textContent = "Guten Abend";
+}
