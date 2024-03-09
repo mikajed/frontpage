11111e8bf160d06988fefab411176687694c1e17
Michael Jedrzejczyk <michael@mikajed.dev>
2024-03-09 23:45:48 +0100

    change date

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,9 @@
-// Uhrzeit
-let span = document.getElementById("date");
-let date = new Date();
-
-if (span && span.textContent === "") {
-  span.innerText = date.toLocaleTimeString();
+function showTime() {
+  let date = document.getElementById("date");
+  date.innerText = new Date().toLocaleTimeString();
 }
 
-setInterval(myTimer, 1000);
-
-function myTimer() {
-  let date = new Date();
-  span.innerText = date.toLocaleTimeString();
-}
+setInterval(showTime, 1000);
 
 // Begrüßung
 const jetzt = new Date();
@@ -19,13 +11,13 @@ const stunde = jetzt.getHours();
 
 if (stunde >= 5 && stunde < 12) {
   document.getElementById("greeting").textContent = "Guten Morgen";
-  document.querySelector('.me').style.color = randomColors();
+  document.querySelector(".me").style.color = randomColors();
 } else if (stunde >= 12 && stunde < 18) {
   document.getElementById("greeting").textContent = "Guten Tag";
-  document.querySelector('.me').style.color = randomColors();
+  document.querySelector(".me").style.color = randomColors();
 } else if (stunde >= 18 && stunde < 22) {
   document.getElementById("greeting").textContent = "Guten Abend";
-  document.querySelector('.me').style.color = randomColors();
+  document.querySelector(".me").style.color = randomColors();
 } else {
   document.getElementById("greeting").textContent = "Gute Nacht";
 }
@@ -37,84 +29,85 @@ if (stunde >= 5 && stunde < 12) {
 // }
 
 // random background image
-document.addEventListener('DOMContentLoaded', function () {
-  const body = document.querySelector('body');
+document.addEventListener("DOMContentLoaded", function () {
+  const body = document.querySelector("body");
   const imgSrc = [
-    './src/images/randomImages/bg1.webp',
-    './src/images/randomImages/bg2.webp',
-    './src/images/randomImages/bg3.webp',
-    './src/images/randomImages/bg4.webp',
-    './src/images/randomImages/bg5.webp',
-    './src/images/randomImages/bg6.webp',
-    './src/images/randomImages/bg7.webp',
-    './src/images/randomImages/bg8.webp',
-    './src/images/randomImages/bg9.webp',
-    './src/images/randomImages/bg10.webp',
+    "./src/images/randomImages/bg1.webp",
+    "./src/images/randomImages/bg2.webp",
+    "./src/images/randomImages/bg3.webp",
+    "./src/images/randomImages/bg4.webp",
+    "./src/images/randomImages/bg5.webp",
+    "./src/images/randomImages/bg6.webp",
+    "./src/images/randomImages/bg7.webp",
+    "./src/images/randomImages/bg8.webp",
+    "./src/images/randomImages/bg9.webp",
+    "./src/images/randomImages/bg10.webp",
   ];
-  
+
   const randomImg = Math.floor(Math.random() * imgSrc.length);
 
   body.style.backgroundImage = `url('${imgSrc[randomImg]}')`;
-  body.style.backgroundSize = 'cover';
+  body.style.backgroundSize = "cover";
 });
 
 // color changer
 function colorChanger() {
-  document.querySelector('.me').style.color = randomColors();
-  document.querySelector('p').style.color = randomColors();
+  document.querySelector(".me").style.color = randomColors();
+  document.querySelector("p").style.color = randomColors();
 }
 
 function randomColors() {
-  return 'hsl(' + (360 * Math.random()) + ',50%,50%)';
+  return "hsl(" + 360 * Math.random() + ",50%,50%)";
 }
 
 // wetter
 let apiKey = config.MY_KEY;
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lang=de&q';
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=de&q";
 
-const searchBar = document.querySelector('.search input');
-const searchBtn = document.querySelector('.search button');
-const weatherIcon = document.querySelector('.weather-icon');
-const errorDisplay = document.querySelector('.error');
-const weatherDisplay = document.querySelector('.weather');
-const cityDisplay = document.querySelector('.city');
-const tempDisplay = document.querySelector('.temp');
-const humidityDisplay = document.querySelector('.humidity');
-const windDisplay = document.querySelector('.wind');
+const searchBar = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+const errorDisplay = document.querySelector(".error");
+const weatherDisplay = document.querySelector(".weather");
+const cityDisplay = document.querySelector(".city");
+const tempDisplay = document.querySelector(".temp");
+const humidityDisplay = document.querySelector(".humidity");
+const windDisplay = document.querySelector(".wind");
 
 function displayWeather(data) {
   console.log(data);
 
   cityDisplay.innerHTML = data.name;
-  tempDisplay.innerHTML = Math.floor(data.main.temp) + '°C';
-  humidityDisplay.innerHTML = data.main.humidity + '%';
-  windDisplay.innerHTML = data.wind.speed + ' km/h';
+  tempDisplay.innerHTML = Math.floor(data.main.temp) + "°C";
+  humidityDisplay.innerHTML = data.main.humidity + "%";
+  windDisplay.innerHTML = data.wind.speed + " km/h";
 
   switch (data.weather[0].main) {
-    case 'Clouds':
-      weatherIcon.src = 'src/images/clouds.png';
+    case "Clouds":
+      weatherIcon.src = "src/images/clouds.png";
       break;
-    case 'Clear':
-      weatherIcon.src = 'src/images/clear.png';
+    case "Clear":
+      weatherIcon.src = "src/images/clear.png";
       break;
-    case 'Rain':
-      weatherIcon.src = 'src/images/rain.png';
+    case "Rain":
+      weatherIcon.src = "src/images/rain.png";
       break;
-    case 'Drizzle':
-      weatherIcon.src = 'src/images/drizzle.png';
+    case "Drizzle":
+      weatherIcon.src = "src/images/drizzle.png";
       break;
-    case 'Mist':
-      weatherIcon.src = 'src/images/mist.png';
+    case "Mist":
+      weatherIcon.src = "src/images/mist.png";
       break;
   }
 
-  weatherDisplay.style.display = 'block';
-  errorDisplay.style.display = 'none';
+  weatherDisplay.style.display = "block";
+  errorDisplay.style.display = "none";
 }
 
 function handleFetchError() {
-  errorDisplay.style.display = 'block';
-  weatherDisplay.style.display = 'none';
+  errorDisplay.style.display = "block";
+  weatherDisplay.style.display = "none";
 }
 
 function checkWeather(city) {
@@ -123,30 +116,30 @@ function checkWeather(city) {
     return;
   }
   fetch(`${apiUrl}=${city}&appid=${apiKey}`)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         handleFetchError();
       } else {
         return response.json();
       }
     })
-    .then(data => {
+    .then((data) => {
       if (data) {
         displayWeather(data);
       }
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
       handleFetchError();
     });
 }
 
-searchBar.addEventListener('keyup', event => {
-  if (event.key === 'Enter') {
+searchBar.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
     checkWeather(searchBar.value);
   }
 });
 
-searchBtn.addEventListener('click', () => {
+searchBtn.addEventListener("click", () => {
   checkWeather(searchBar.value);
 });
